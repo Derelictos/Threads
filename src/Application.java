a9ccde4da652143088402f4ca2e82a176448540c
Derelictos <zheny20132@gmail.com>
2023-10-04 14:42:58 +0300

    creating new thread for pract

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,12 @@
-public class Application {
-    public static void main(String[] args) {
+public class Application implements Runnable {
 
+    @Override
+    public void run() {
+        System.out.println("Hello my new Thread:D");
     }
+    public static void main(String[] args) {
+        (new Thread (new Application())).start();
+    }
+
 }
+
