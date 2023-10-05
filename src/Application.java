630ddd15f261823b64716028152d61b385954f42
Derelictos <zheny20132@gmail.com>
2023-10-06 01:50:20 +0300

    Add some theory about Threads

--- src/Application.java
+++ src/Application.java
@@ -1,16 +1,29 @@
-public class Application implements Runnable {
+public class Application {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread1 = new Thread(myRunnable);
+        thread1.start();
+    }
+}
 
-    @Override
+class MyThread extends Thread {
     public void run() {
-        for(;;) {
-            System.out.println("Hello my new Thread:D");
-
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Поток класса Thread " + Thread.currentThread().getId() + ": " + i);
         }
     }
-
-    public static void main(String[] args) {
-        (new Thread (new Application())).start();
-    }
-
 }
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int j = 1; j <= 10; j++){
+            System.out.println("Поток интерфейса Runnable " + Thread.currentThread().getId() + ": " + j);
+        }
+    }
+}
+
+
 
---
+++ src/StudentData.java
@@ -0,0 +1,6 @@
+public class StudentData {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
