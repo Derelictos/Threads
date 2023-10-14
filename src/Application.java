2873227a77441b0144ee7703176720eb55773d14
Derelictos <zheny20132@gmail.com>
2023-10-14 22:21:31 +0300

    final fix

--- src/Application.java
+++ src/Application.java
@@ -10,33 +10,36 @@ public class Application {
         System.out.println("Write\n1. To see theory about threads\n" +
                 "2. Find the area of a square with the side of the number you entered\n" +
                 "3. Run the administrator Thread\n" +
-                "4....\n" +
                 "0. To leave");
         Scanner scan = new Scanner(System.in);
         int variant = scan.nextInt();
         ThreadPoolTheory threadPoolTheory = new ThreadPoolTheory();
         ThreadPoolAdministrator threadPoolAdministrator = new ThreadPoolAdministrator();
-        switch (variant) {
-            case 1:
-                threadPoolTheory.execute();
-                break;
-            case 2: {
-                int number = 0;
-                System.out.println("What is number: ");
-                number = scan.nextInt();
-                MathSquare squareOfCube = new MathSquare(number);
-                squareOfCube.start();
-                break;
+        try {
+            switch (variant) {
+                case 1:
+                    threadPoolTheory.execute();
+                    break;
+                case 2: {
+                    int number = 0;
+                    System.out.println("What is number: ");
+                    number = scan.nextInt();
+                    MathSquare squareOfCube = new MathSquare(number);
+                    squareOfCube.start();
+                    break;
+                }
+                case 3:
+                    threadPoolAdministrator.executeAdmin();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Wrong number");
+                    break;
             }
-            case 3:
-                threadPoolAdministrator.executeAdmin();
-                break;
-            case 0:
-                break;
-            default:
+        } catch (Exception e) {
+            System.out.println("Smth wrong, try again :<");
         }
-        //Create new fixed threadPool for admins
     }
-
-
+    //Create new fixed threadPool for admins
 }
