ac892f96277f6e281a8153a28c8280d0f2ba77ce
Derelictos <zheny20132@gmail.com>
2023-10-14 19:11:19 +0300

    Add 3 type of Threads

---
+++ src/MathSquare.java
@@ -0,0 +1,25 @@
+public class MathSquare extends Thread{
+
+    private final int value;
+    private int result;
+    public MathSquare(int value) {
+        this.value = value;
+        result = 0;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(50);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        result = value * value;
+        System.out.println(result);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(result);
+    }
+}
